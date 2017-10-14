ff8180009cd46a0d135bc4d078b70c774f3b8976
puzhengsong <1391663722@qq.com>
2017-10-14 22:09:55 +0800

    debug

--- src/main/resources/static/js/borrow.js
+++ src/main/resources/static/js/borrow.js
@@ -1,20 +1,26 @@
 function borrow(id) {
+    var f = 1;
     $.ajax({
         type: 'GET',
         url: '/web/borrowBook?id=' + id,
         dataType: "json",
         async: false,
         success: function (data) {
-
             if(data['error']==2){
+                f = 0;
                 window.location = '/web/login';
                 return;
             }
 
+
         },
         error: function () {
+            return;
         }
     });
+    if(f==0){
+        return;
+    }
     var msg = "亲，确定预约借阅这本书籍么?";
     if(confirm(msg)){
         $.ajax({
@@ -31,9 +37,7 @@ function borrow(id) {
             error: function () {
                 alert("预约失败!");
             }
-
         });
-
     }
 
 }
